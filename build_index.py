4ea025215fdf04d1fdd181fc72254eb71ab1ec8f
anna <anna.guccione@studenti.units.it>
2026-04-21 19:17:08 +0200

    rem: build_forward_index; add: build_doc_vectors

--- build_index.py
+++ build_index.py
@@ -1,14 +1,14 @@
 import json
 import pickle
-from math import log
 from core import Recipe, InvertedIndex, tokenize
+from scipy.sparse import csr_array
 
 def create_recipe_corpus(filename):
     with open(filename, "r") as file:
         data = json.load(file)
     
     recipe_corpus = []
-    doc_id = 1
+    doc_id = 0
     for index in data:
         recipe_json = data[index]
         if recipe_json and recipe_json.get('ingredients') and recipe_json.get('instructions') and recipe_json.get('title'):
@@ -21,42 +21,41 @@ def create_recipe_corpus(filename):
             doc_id += 1
     return recipe_corpus
 
-def build_forward_index(inverted_index):
-    # { doc_id: { term_object: tfidf_weight } }
-    forward_index = {}
-    total_terms = len(inverted_index.index)
-        
-    for i, (term, posting_list) in enumerate(inverted_index.index.items()):
-        idf = term.idf
-        for doc_id, posting in posting_list._map.items():
-            if doc_id not in forward_index:
-                forward_index[doc_id] = {}
-            
-            # TF-IDF
-            forward_index[doc_id][term] = posting.tf * idf
-        
-        if i % 10000 == 0:
-            print(f"Progress: {i}/{total_terms}")
-            
-    return forward_index
+def build_doc_vectors(inverted_index, corpus):
+
+    number_of_terms = len(inverted_index.index)
+    number_of_docs = len(corpus)
+
+    term_to_idx = {term: i for i, term in enumerate(inverted_index.index.keys())}
+
+    # for the doc vectors, we generate a sparse array to avoid wasting space with zeros entries
+    # this data structure instead of saving a #docs x #terms matrix, saves the value of tf-idf and the corresponding coordinates
+    rows, cols, data = [], [], []
+
+    for term, postings in inverted_index.index.items():
+        for posting in postings:
+            rows.append(posting.doc_id)
+            cols.append(term_to_idx[term])
+            data.append(posting.tf * term.idf)
+
+    return csr_array((data, (rows, cols)), shape=(number_of_docs, number_of_terms))
 
 if __name__ == "__main__":
-    # 1. Creazione del Corpus
+
     print("Loading corpus...")
     corpus = create_recipe_corpus("recipes/recipes.json")
-    
-    # 2. Costruzione e salvataggio Inverted Index
+
     print("Building Inverted Index...")
     idx = InvertedIndex(corpus)
-    
+
+    print("Building Document Vectors...")
+    doc_vectors = build_doc_vectors(idx, corpus)
+
+    # saves the inverted index and the document vectors in a pickle file, to use them in testing
     with open("inverted_index.pkl", "wb") as f:
         pickle.dump(idx, f)
     print("Inverted Index saved in 'inverted_index.pkl'")
 
-    # 3. Costruzione e salvataggio Forward Index
-    print("Generating Foward Index...")
-    recipe_vectors = build_forward_index(idx)
-    
-    with open("forward_index.pkl", "wb") as f:
-        pickle.dump(recipe_vectors, f)
-    print("Forward Index saved in 'forward_index.pkl'")
+    with open("doc_vectors.pkl", "wb") as f:
+        pickle.dump(doc_vectors, f)
+    print("Document Vectors saved in 'doc_vectors.pkl'")
