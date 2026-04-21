b2b9a56ce75d68d6876395400e31e994cb9955bd
anna <anna.guccione@studenti.units.it>
2026-04-21 19:18:47 +0200

    rem: forward_index

--- testing.py
+++ testing.py
@@ -1,12 +1,9 @@
 import pickle
 from core import Term, PostingList, Posting
 from itertools import islice
-with open("forward_index.pkl", "rb") as file:
-    recipe_vectors = pickle.load(file)
 with open("inverted_index.pkl", "rb") as file:
     idx = pickle.load(file)
 
-print(recipe_vectors[1])
 
 count = 0
 for term, posting_list in idx.index.items():
