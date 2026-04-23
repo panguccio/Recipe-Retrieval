268d2c78a6225a17caab752cd47682aed3398c63
Marrenk <federicomarenco02@gmail.com>
2026-04-23 13:20:59 +0200

    update ranking logic for retrieval

--- query.py
+++ query.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pickle
+import heapq
 from core import Term, tokenize, cosine_similarity
 from build_index import create_recipe_corpus
-
 from scipy.sparse import csr_array
 
 
@@ -15,11 +15,11 @@ def query_to_vector(query, index, vocab):
             term = Term(token, zone)
             if term in index:
                 vector[vocab[term]] += 1
-                print(term)
     norma = np.linalg.norm(vector)
     if norma > 0:
         vector = vector / norma
     return csr_array(vector)
+
 
 if __name__ == "__main__":
 
@@ -28,27 +28,37 @@ if __name__ == "__main__":
 
     with open("doc_vectors.pkl", "rb") as file:
         doc_vect = pickle.load(file)
-    
+
     vocab = idx.vocab
 
     corpus = create_recipe_corpus("recipes/recipes.json")
 
     while True:
         query = input("\n Enter a query (or 'q' to quit): ").strip()
-        if query.lower() == 'q':
+        if query.lower() == "q":
             break
+
         vec_q = query_to_vector(query, idx, vocab)
-        
-        list_cos_sim = {}
-        
+
+        list_cos = []
+        K = 5
+
         for i, vect in enumerate(doc_vect):
             cos_sim = cosine_similarity(vec_q, vect)
-            list_cos_sim[i] = cos_sim
-        
-        list_cos_sim = sorted(list_cos_sim.items(), key=lambda x: x[1], reverse=True)
+
+            if len(list_cos) < K:
+                heapq.heappush(list_cos, (cos_sim, i))
+            else:
+                if cos_sim > list_cos[0][0]:
+                    heapq.heappop(list_cos)
+                    heapq.heappush(list_cos, (cos_sim, i))
+
+    
+        list_cos = sorted(list_cos, key=lambda x: x[0], reverse=True)
         print("\nTop 5 most similar documents:")
-        for doc_id, sim in list_cos_sim[:5]:
-            print(f"Document ID: {doc_id}, Cosine Similarity: {sim}")
+        for cos_sim, doc_id in list_cos:
+            print(f"Document ID: {doc_id}, Cosine Similarity: {cos_sim}")
             print(f"Recipe: {corpus[doc_id]}")
+
         
-        
+
