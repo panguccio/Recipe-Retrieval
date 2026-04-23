df2f6e375e464d729aa99bee3657aa47bcdde2b4
Gabriele Pasqualini <gabrielepasqualini@outlook.com>
2026-04-23 20:04:20 +0200

    created method search_top_n

--- query.py
+++ query.py
@@ -26,6 +26,14 @@ def query_to_vector(query, index, vocab):
     return csr_array(vector)
 
 
+def search_top_n(query, n):
+    vec_q = query_to_vector(query, idx, vocab)
+    similarities = vec_q.dot(doc_vect.T).toarray().flatten()
+    top_n_indices = np.argsort(similarities)[::-1][:n]
+
+    return top_n_indices, similarities
+
+
 if __name__ == "__main__":
 
     with open("inverted_index.pkl", "rb") as file:
@@ -35,27 +43,21 @@ if __name__ == "__main__":
         doc_vect = pickle.load(file)
 
     vocab = idx.vocab
-
     corpus = load_corpus("recipes/recipes.json")
 
     # Addresses lazy initialization that makes the first query very slow
     dummy_warmup = query_to_vector(
         "test query", idx, vocab).dot(doc_vect.T).toarray()
 
+
     while True:
         query = input("\n Enter a query (or 'q' to quit): ").strip()
         if query.lower() == "q":
             break
 
-        vec_q = query_to_vector(query, idx, vocab)
-
-        similarities = vec_q.dot(doc_vect.T).toarray().flatten()
-
-        top_5_indices = np.argsort(similarities)[::-1][:5]
-
+        top_n_indices, similarities = search_top_n(query, 10)
         print("\nTop 5 most similar documents:")
-        for doc_id in top_5_indices:
 
-            print(
-                f"Document ID: {doc_id}, Cosine Similarity: {similarities[doc_id]}")
+        for doc_id in top_n_indices:
+            print(f"Document ID: {doc_id}, Cosine Similarity: {similarities[doc_id]}")
             print(f"Recipe: {corpus[f"{doc_id}"]["title"]}")
