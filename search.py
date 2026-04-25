1f7ae89423f20f211a13bb12f003b5b201c88d72
Gabriele <gabrielepasqualini@outlook.com>
2026-04-25 12:08:52 +0200

    added apply_relevance_feedback and set_weights

--- search.py
+++ search.py
@@ -1,22 +1,20 @@
 import numpy as np
 import pickle
-from core import Term, tokenize
+from core import Term, tokenize, rochio_algorithm # Assicurati di importare rochio qui
 from scipy.sparse import csr_array
 
 class SearchEngine:
-    def __init__(self, index_path, doc_vect, weights=None):
+    def __init__(self, index_path, doc_vect_path, weights=None):
         print("Caricamento Search Engine...")
         with open(index_path, "rb") as f:
             self.idx = pickle.load(f)
-        with open(doc_vect, "rb") as f:
+        with open(doc_vect_path, "rb") as f:
             self.doc_vect = pickle.load(f)
         
         self.vocab = self.idx.vocab
-
-        #Assegnazione pesi
-        self.weights = weights or {"title": 1, "instructions": 0, "ingredients": 1}
+        self.weights = weights or {"title": 0.7, "instructions": 0.1, "ingredients": 0.2}
         
-        # Warmup perchè python è pigro :)
+        # Warmup per python che è un po pigro
         salamuccio = self.query_to_vector("warmup").dot(self.doc_vect.T)
 
     def query_to_vector(self, query):
@@ -33,8 +31,19 @@ class SearchEngine:
             vector /= norma
         return csr_array(vector)
 
-    def search(self, query, n=10):
-        vec_q = self.query_to_vector(query)
+    def search(self, vec_q, n=10):
         similarities = vec_q.dot(self.doc_vect.T).toarray().flatten()
         top_n_indices = np.argsort(similarities)[::-1][:n]
         return top_n_indices, similarities
+
+    def apply_relevance_feedback(self, vec_q, relevant_ids, all_top_ids):
+        relevant_vecs = self.doc_vect[relevant_ids]
+        non_relevant_ids = [idx for idx in all_top_ids if idx not in relevant_ids]
+        non_relevant_vecs = self.doc_vect[non_relevant_ids]
+        
+        # Chiama la funzione rochio_algorithm definita in core.py
+        return rochio_algorithm(vec_q, relevant_vecs, non_relevant_vecs)
+    
+    def set_weights(self, new_weights):
+        """Cambia i pesi senza ricaricare l'indice"""
+        self.weights = new_weights
