21dde21a312eee8e056b0defe98555f69709b2b5
Marrenk <federicomarenco02@gmail.com>
2026-04-30 18:37:55 +0200

    refactored app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 import yaml
 from src.utils.corpus_parser import load_corpus
 from src.search.engine import SearchEngine
-from src.search.query import print_results
 
 
 app = Flask(__name__)
@@ -17,54 +16,16 @@ engine = SearchEngine(
     config['paths']['vectors_bin'],
     weights=config['settings']['default_weights']
 )
+
 corpus = load_corpus(config['corpus']['clean'])
 
 top_n_indices = None
 vec_q = None
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+def format_results(similarity):
     global top_n_indices, vec_q
+    formatted_result = []
     
-    risultati_per_html = []
-    query = None
-
-    if request.method == 'POST':
-        query = request.form.get('search')
-        if query:
-            vec_q = engine.query_to_vector(query)
-            top_n_indices, similarities = engine.search(vec_q, n=10)
-
-            for doc_id in top_n_indices:
-                ricetta = corpus.get(str(doc_id), {})
-
-                raw_ingredients = ricetta.get('ingredients', [])
-
-                lista_ingredienti = [line.strip() for line in raw_ingredients.splitlines() if line.strip()]
-
-                risultati_per_html.append({
-                    'id': doc_id,
-                    'titolo': ricetta.get('title', 'Titolo non trovato'),
-                    'score': round(float(similarities[doc_id]), 4),
-                    'ingredienti': lista_ingredienti,
-                    'procedimento': ricetta.get('instructions', [])
-                })
-    return render_template('index.html', risultati=risultati_per_html, query=query)
-        
-@app.route('/submit-feedback', methods=['POST'])
-def handle_feedback():
-    global top_n_indices, vec_q
-    risultati_per_html = []
-    
-    id_string = request.form.get('feedback_ids', '')
-
-
-    relevant_input = id_string.strip().split()
-    relevant_doc_ids = [int(d) for d in relevant_input]
-
-    vec_q = engine.apply_relevance_feedback(vec_q, relevant_doc_ids, top_n_indices)
-    top_n_indices, similarities = engine.search(vec_q, n=10)
-
     for doc_id in top_n_indices:
         ricetta = corpus.get(str(doc_id), {})
         
@@ -72,14 +33,49 @@ def handle_feedback():
 
         lista_ingredienti = [line.strip() for line in raw_ingredients.splitlines() if line.strip()]
 
-        risultati_per_html.append({
+        formatted_result.append({
             'id': doc_id,
             'titolo': ricetta.get('title', 'Titolo non trovato'),
-            'score': round(float(similarities[doc_id]), 4),
+            'score': round(float(similarity[doc_id]), 4),
             'ingredienti': lista_ingredienti,
             'procedimento': ricetta.get('instructions', [])
         })
-    return render_template('index.html', risultati=risultati_per_html)
+
+    return formatted_result
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    global top_n_indices, vec_q
+    query = None
+    formatted_results = []
+
+    if request.method == 'POST':
+        query = request.form.get('search')
+        if query:
+            vec_q = engine.query_to_vector(query)
+            top_n_indices, similarities = engine.search(vec_q, n=10)
+
+            formatted_results = format_results(similarities)
+
+    return render_template('index.html', risultati=formatted_results, query=query)
+        
+@app.route('/submit-feedback', methods=['POST'])
+def handle_feedback():
+    global top_n_indices, vec_q
+    
+    id_string = request.form.get('feedback_ids', '')
+
+    relevant_input = id_string.strip().split()
+    relevant_doc_ids = [int(d) for d in relevant_input]
+
+    vec_q = engine.apply_relevance_feedback(vec_q, relevant_doc_ids, top_n_indices)
+    top_n_indices, similarities = engine.search(vec_q, n=10)
+
+    formatted_results = format_results(similarities)
+
+    return render_template('index.html', risultati=formatted_results)
     
     
 
